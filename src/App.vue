1c6b60cd6eb5784207337a5de6c60ee20f94243f
nanakomatasu <chendongdi6@gmail.com>
2023-02-15 16:51:00 +0800

    课堂完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <span>全选:</span>
+    <input type="checkbox" v-model="isAll"/>
+    <button @click="btn">反选</button>
+    <ul>
+      <li v-for="(item,index) in arr" :key="index">
+        <input type="checkbox" v-model="item.c"/>
+        <span>{{item.name}}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      arr: [
+        {
+          name: "猪八戒",
+          c: false,
+        },
+        {
+          name: "孙悟空",
+          c: true,
+        },
+        {
+          name: "唐僧",
+          c: false,
+        },
+        {
+          name: "白龙马",
+          c: false,
+        },
+      ],
+    };
+  },
+  computed: {
+    isAll: {
+      set(val){
+        this.arr.forEach(item => {
+          item.c = val
+        })
+      },
+      get(){
+        return this.arr.every(obj => obj.c === true)
+      }
+    }
+  },
+  methods:{
+    btn(){
+      this.arr.forEach(item => {
+        item.c =!item.c
+      })
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/计算属性.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div>
+        <span>姓:</span>
+        <input type="text" v-model="fName">
+    </div>
+    <div>
+        <span>名:</span>
+        <input type="text" v-model="sName">
+    </div>
+    <div>
+        <span>姓名:</span>
+        <input type="text" v-model="full">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MytestHelloWorld',
+
+  data() {
+    return {
+      fName: '',
+      sName: ''
+    };
+  },
+  computed: {
+    // full() { // 只读，只可以取值
+    //   return this.fName + '.'+ this.sName
+    // }
+    full: {// 即可以取值，又可以设置值
+      get(){
+        return this.fName + '.'+ this.sName
+      },
+      set(val){ 
+        this.fName = val && val.split('.')[0] || []
+        this.sName = val && val.split('.')[1] || []
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+input{
+  border: 1px solid #ccc;
+}
+</style>
