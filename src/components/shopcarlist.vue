438991d411ea5607df5256f645f12f34a0caafd0
nanakomatasu <chendongdi6@gmail.com>
2023-02-18 00:17:13 +0800

    更换文件名称

---
+++ src/components/shopcarlist.vue
@@ -0,0 +1,77 @@
+<template>
+   
+        <tr>
+          <td><input type="checkbox" v-model="check"> </td>
+          <td>{{name}}</td>
+          <td>{{price}}</td>
+          <td> <button @click="jian">-</button>{{num}} <button @click="add">+</button></td>
+          <td>{{sum}}</td>
+          <td><button @click="del">删除</button></td>
+        </tr>
+   
+</template>
+
+<script>
+export default {
+    name:'listVue',
+    props: {
+        name:{
+            type:String,
+            
+        },
+        price:{
+            type:Number
+        },
+        num:{
+            type:Number
+        },
+        checked:{
+            type:Boolean
+        }
+    },
+    data() {
+        return {
+          check:this.checked
+        };
+    },
+    computed: {
+        sum(){
+        return this.num*this.price
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    watch: {
+        check(new1){
+         this.$emit('check',new1,this.name)
+        },
+        checked(new1){
+            // console.log(new1);
+            this.check=new1
+        }
+        
+    },
+    methods: {
+        add(){
+            this.$emit('add',1,this.name)
+        },
+        jian(){
+            this.$emit('jian',this.name)
+        },
+        del(){
+            this.$emit('del',this.name)
+        }
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/sum.vue
@@ -0,0 +1,46 @@
+<template>
+  <tfoot>
+        <tr>
+          <td>合计:</td>
+          <td colspan="5">{{ sum }}</td>
+        </tr>
+      </tfoot>
+</template>
+
+<script>
+export default {
+  name:'sumVue',
+    props: {
+        sum:{
+            type:Number
+        }
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+       
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    watch: {
+
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
